dfe78b98cc2d846e6ca3141c9d8254a0f661ac7d
Murilo Aparecido Carvalho Nascimento <murilo.aparecido@ufms.br>
2023-10-11 19:35:53 -0300

    Implementação -> Módulo Telefone

--- src/telefone/dto/create-telefone.dto.ts
+++ src/telefone/dto/create-telefone.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTelefoneDto {}
+import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { TipoTelefone } from '../entities/telefone.entity';
+
+export class CreateTelefoneDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  numero: string;
+
+  @IsEnum(TipoTelefone)
+  @IsNotEmpty()
+  tipo: string;
+}
--- src/telefone/entities/telefone.entity.ts
+++ src/telefone/entities/telefone.entity.ts
@@ -1,1 +1,21 @@
-export class Telefone {}
+import { Column, Entity } from 'typeorm';
+
+export enum TipoTelefone {
+  PESSOAL = 'Pessoal',
+  PROFISSIONAL = 'Profissional',
+  FUNCIONAL = 'Funcional',
+  UNIDADE = 'Unidade',
+}
+
+@Entity()
+export class Telefone {
+  @Column()
+  numero: string;
+
+  @Column({
+    type: 'varchar',
+    enum: TipoTelefone,
+    default: TipoTelefone.PESSOAL,
+  })
+  tipo: TipoTelefone;
+}
