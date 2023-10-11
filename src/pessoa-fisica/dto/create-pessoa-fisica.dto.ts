acadb805dea981f7e074404f91961a6bc35e60a4
MuriloApa <100038314+MuriloApa@users.noreply.github.com>
2023-10-11 12:28:31 -0300

    Adição do diagrama de classes

--- src/pessoa-fisica/dto/create-pessoa-fisica.dto.ts
+++ src/pessoa-fisica/dto/create-pessoa-fisica.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePessoaFisicaDto {}
+import { IsDate, IsString, Length, MinLength } from 'class-validator';
+
+export class CreatePessoaFisicaDto {
+  @IsString()
+  @Length(11)
+  cpf: string;
+
+  @IsString()
+  @MinLength(3)
+  nome: string;
+
+  @IsDate()
+  dataNascimento: Date;
+
+  @IsString()
+  @MinLength(3)
+  sexo: string;
+
+  @IsString()
+  @MinLength(3)
+  genero: string;
+
+  @IsString()
+  @Length(2)
+  tipoSanguineo: string;
+}
