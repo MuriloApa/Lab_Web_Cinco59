498a147905cc9acce514fe38b986d94f0372609f
MuriloApa <100038314+MuriloApa@users.noreply.github.com>
2023-10-16 23:26:39 -0300

    Trabalho antes do swagger

--- src/genero/dto/create-genero.dto.ts
+++ src/genero/dto/create-genero.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateGeneroDto {
+  @ApiProperty()
   @IsString()
   @MinLength(3)
   @IsNotEmpty()
